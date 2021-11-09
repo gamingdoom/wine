f4a95d278a65eb876b3cfde07aff6b49d99bdb26
tannisroot <10602045+tannisroot@users.noreply.github.com>
2021-11-09 22:35:47 +0300

    lutris-fshack-6.21-rc2, generated with Tk-Glitch/PKGBUILDS

--- dlls/mountmgr.sys/device.c
+++ dlls/mountmgr.sys/device.c
@@ -1914,6 +1914,19 @@ static NTSTATUS query_property( struct disk_device *device, IRP *irp )
 
         break;
     }
+    case StorageDeviceSeekPenaltyProperty:
+    {
+        DEVICE_SEEK_PENALTY_DESCRIPTOR *descriptor;
+        FIXME( "Faking StorageDeviceSeekPenaltyProperty data\n" );
+        memset( irp->AssociatedIrp.SystemBuffer, 0, irpsp->Parameters.DeviceIoControl.OutputBufferLength );
+        descriptor = irp->AssociatedIrp.SystemBuffer;
+        descriptor->Version = sizeof(DEVICE_SEEK_PENALTY_DESCRIPTOR);
+        descriptor->Size = sizeof(DEVICE_SEEK_PENALTY_DESCRIPTOR);
+        /* Assume no penalty, SSDs are common enough */
+        descriptor->IncursSeekPenalty = FALSE;
+        status = STATUS_SUCCESS;
+        break;
+    }
     default:
         FIXME( "Unsupported property %#x\n", query->PropertyId );
         status = STATUS_NOT_SUPPORTED;
